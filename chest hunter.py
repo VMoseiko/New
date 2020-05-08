31767627e4f1b2bf2c61458d42c338007d2f0638
Nefros <moseykovlad@gmail.com>
2020-05-08 15:24:53 +0300

    продолжение

--- chest hunter.py
+++ chest hunter.py
@@ -2,6 +2,7 @@ import sys
 import math
 import random
 
+
 def get_new_board():
     # создаем игровое поле размером 60*15 , со случайными волнами
     board = []
@@ -39,6 +40,49 @@ def draw_board(board):
     print('  ' + tens_distance_line)
     print('  ' + '0123456789' * 6)
     print()
+
+
+def get_random_chest(num_chest):
+    chest = []
+    while len(chest) < num_chest:
+        new_chest = [random.randint(0, 59), random.randint(0, 14)]
+        if new_chest not in chest:
+            chest.append(new_chest)
+    return chest
+
+
+def is_on_board(x, y):
+    return 0 < x < 59 and 0 < y < 59
+
+
+def make_move(chests, board, x, y):
+    small_distance = 100
+    for cx, cy in chests:
+        distance = math.sqrt((cx-x)**2 + (cy-y)**2)
+
+        if distance < small_distance:
+            small_distance = distance
+
+    if small_distance == 0:
+        chests.remove([x, y])
+        return 'вы нашли сундук с сокровищами!'
+
+    else:
+        if small_distance <= 10:
+            board[x, y] = str(small_distance)
+            return 'сундук с сокровищами обнружен на растоянии %s от гидролокатора' % str(small_distance)
+        else:
+            board[x, y] = 'X'
+            return 'сундук не зоны видимости гидролокатора'
+
+
+
+
+
+
+
+
+
 
 
 
